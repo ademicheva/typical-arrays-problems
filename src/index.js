2e4633a3b953312087e798caf0f80471e1eb226e
Алина <alina@MacBook-Pro-Alina.local>
2021-02-25 20:08:16 +0200

    init: start typical-arrays-problems tast

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+exports.min = function min(array) {
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return 0;
+};
